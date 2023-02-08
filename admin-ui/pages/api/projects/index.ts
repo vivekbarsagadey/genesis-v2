e3685358dab949766aac271ec0aa0cb2e3162c4b
neha <vivek.bnb@gmail.com>
2023-02-08 18:28:29 +0530

    updated add api folder

---
+++ admin-ui/pages/api/projects/index.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+
+import prisma from "../../../utils/prisma";
+import { handleApiMiddleware } from "../middleware";
+
+
+// POST /api/project
+const handle = async (req: NextApiRequest, res: NextApiResponse) => {
+  await handleApiMiddleware(req, res);
+  if (req.method === "POST") {
+    const result = await prisma.project.create({
+      data: req.body,
+    }); 
+    res.json(result?.id);
+  } else if (req.method === "GET") {
+    const projects = await prisma.project.findMany();
+    res.json(projects);
+  } else {
+    res.status(404).send({ message: "Method is not supported" });
+  }
+};
+
+
+
+export default handle;
