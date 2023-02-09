819a1ba53c5ccfbfa4d83c762b46a6405e71349e
Saurabh Chandel <saurabh.c@whizit.co.in>
2023-02-09 14:18:50 +0530

    updating branch web

--- admin-ui/utils/xlsx-util.ts
+++ admin-ui/utils/xlsx-util.ts
@@ -1,6 +1,5 @@
-import * as FileSaver from "file-savesr";
+// import * as FileSaver from "file-savesr";
 import * as XLSX from "xlsx";
-
 
 interface xlsxProps {
   items: any[];
@@ -15,7 +14,7 @@ const xlsxDownload = async ({ items, fileName }: xlsxProps) => {
   const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
   const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
   const data = new Blob([excelBuffer], { type: fileType });
-  FileSaver.saveAs(data, fileName + fileExtension);
+  // FileSaver.saveAs(data, fileName + fileExtension);
 };
 
 export { xlsxDownload };
