c9866592e9517e20157fc6720d7fadf1fe6556ca
Pranali <pranali.b@whizit.co.in>
2023-02-15 11:42:31 +0530

    resolving merge conflicts

--- admin-ui/utils/xlsx-util.ts
+++ admin-ui/utils/xlsx-util.ts
@@ -1,20 +1,20 @@
 // import * as FileSaver from "file-savesr";
-import * as XLSX from "xlsx";
+// import * as XLSX from "xlsx";
 
-interface xlsxProps {
-  items: any[];
-  fileName: string;
-}
-const fileType =
-  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-const fileExtension = ".xlsx";
+// interface xlsxProps {
+//   items: any[];
+//   fileName: string;
+// }
+// const fileType =
+//   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+// const fileExtension = ".xlsx";
 
-const xlsxDownload = async ({ items, fileName }: xlsxProps) => {
-  const ws = XLSX.utils.json_to_sheet(items);
-  const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
-  const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-  const data = new Blob([excelBuffer], { type: fileType });
-  // FileSaver.saveAs(data, fileName + fileExtension);
-};
+// const xlsxDownload = async ({ items, fileName }: xlsxProps) => {
+//   const ws = XLSX.utils.json_to_sheet(items);
+//   const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+//   const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+//   const data = new Blob([excelBuffer], { type: fileType });
+//   FileSaver.saveAs(data, fileName + fileExtension);
+// };
 
-export { xlsxDownload };
+// export { xlsxDownload };
