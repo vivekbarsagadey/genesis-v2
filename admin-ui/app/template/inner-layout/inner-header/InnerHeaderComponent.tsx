542e6bc0fc8951806b575d81d2a25ef060b1dd43
vivek <vivek.bnb@gmail.com>
2023-02-22 10:14:07 +0530

    fix location issue

--- admin-ui/app/template/inner-layout/inner-header/InnerHeaderComponent.tsx
+++ admin-ui/app/template/inner-layout/inner-header/InnerHeaderComponent.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect } from "react";
 import { Grid, IconButton, Button } from "@mui/material";
 import Stack from "@mui/material/Stack";
--- admin-ui/component/common/Header/HeaderComponent.tsx
+++ admin-ui/component/common/Header/HeaderComponent.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Box, Button, Grid, IconButton, Typography } from "@mui/material";
 import Stack from "@mui/material/Stack";
