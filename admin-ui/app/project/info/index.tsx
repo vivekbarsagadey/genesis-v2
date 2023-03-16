1b2bc7ebf7185553b5ffdf9597d25263e6858203
neha <vivek.bnb@gmail.com>
2023-03-16 17:02:09 +0530

    working on Project Grid Layout

--- admin-ui/app/project/info/index.tsx
+++ admin-ui/app/project/info/index.tsx
@@ -13,6 +13,7 @@ import {
 import Checkbox from "@mui/material/Checkbox";
 import Link from "next/link";
 import ICompany from "../../company/company.model";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 
 type CompanyComponentProps = {
   items: any;
@@ -20,41 +21,53 @@ type CompanyComponentProps = {
 
 const InfoProjectComponent = ({ items }: CompanyComponentProps) => {
   return (
-    <Box mt={0.6} mr={2} >
+    <Box mr={2} mt={0.6}>
       <Paper variant="outlined">
-        <Grid container spacing={4}>
-          <Grid item xs={0.12}></Grid>
-          <Grid item xs={0.488}>
-            <Checkbox size="small" />
+        <Grid container>
+          <Grid item xs={2} display={"flex"} justifyContent={"flex-end"}>
+            <Grid container ml={1}>
+              <Grid item xs={4}>
+                <IconButton>
+                  <CheckBoxOutlineBlankIcon fontSize="small" />
+                </IconButton>
+              </Grid>
+              <Grid item xs={6}>
+                <IconButton>
+                  <RemoveRedEyeIcon fontSize="small" />
+                </IconButton>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={1} style={{ display: "flex" }}>
-            <IconButton>
-              <RemoveRedEyeIcon fontSize="small" />
-            </IconButton>
+
+          <Grid item xs={3}>
+            <Typography variant="body2" noWrap>
+              {items.name}
+            </Typography>
           </Grid>
-          <Grid item xs={3} sm={3}>
-            <Typography variant="body2">{items.name}</Typography>
+          <Grid item xs={3}>
+            <Typography variant="body2" noWrap>
+              {items.customerName}
+            </Typography>
           </Grid>
-          <Grid item xs={3} sm={3} spacing={1}>
-            <Typography variant="body2">{items.customerName}</Typography>
-          </Grid>
-          <Grid item xs={2.2} sm={2.9}>
-            <Typography variant="body2">{items.application}</Typography>
+          <Grid item xs={2} mr={7}>
+            <Typography variant="body2" noWrap>
+              {items.application}
+            </Typography>
           </Grid>
 
           <Grid item xs={1}>
             <Grid container>
-              <Grid item xs={3.8}>
-                <Link href={"/project/-1"} passHref>
-                  <Tooltip title="Edit">
+              <Grid item xs={4}>
+                <Tooltip title="Edit">
+                  <Link href={"/project/-1"} passHref>
                     <IconButton>
                       <EditIcon fontSize="small" />
                     </IconButton>
-                  </Tooltip>
-                </Link>
+                  </Link>
+                </Tooltip>
               </Grid>
-              <Grid item xs={4}>
-                <Tooltip title="Download">
+              <Grid item xs={2}>
+                <Tooltip title="Delete">
                   <IconButton>
                     <FileDownloadIcon fontSize="small" />
                   </IconButton>
--- admin-ui/app/project/list/index.tsx
+++ admin-ui/app/project/list/index.tsx
@@ -1,14 +1,13 @@
 "use client";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import { Box, Grid, IconButton, Typography } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
 import Pagination from "@mui/material/Pagination";
 import Paper from "@mui/material/Paper";
 import { useState } from "react";
 import InfoProjectComponent from "../info";
 import IProject from "../project.model";
 import projectPagination from "./project.list.pagination";
-
 type ProjectComponentProps = {
   copyProject: Array<IProject>;
   projectSearchList: any;
@@ -31,7 +30,7 @@ const ProjectListComponent = ({
 
   return (
     <>
-      <Box mt={2} mr={2} >
+      {/* <Box mt={2} mr={2} >
         <Paper variant="outlined">
           <Grid container spacing={4}>
             <Grid item xs={0.12}></Grid>
@@ -60,7 +59,55 @@ const ProjectListComponent = ({
             </Grid>
           </Grid>
         </Paper>
+      </Box> */}
+
+      <Box mr={2} mt={2}>
+        <Paper variant="outlined">
+          <Grid container>
+            <Grid item xs={2} display={"flex"} justifyContent={"flex-end"}>
+              <Grid container ml={1}>
+                <Grid item xs={4}>
+                  <IconButton>
+                    <CheckBoxOutlineBlankIcon fontSize="small" />
+                  </IconButton>
+                </Grid>
+                <Grid item xs={6}>
+                  <IconButton>
+                    <RemoveRedEyeIcon fontSize="small" />
+                  </IconButton>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Typography variant="subtitle2" noWrap>
+                Project Name
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="subtitle2" noWrap>
+                Company Name
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography variant="subtitle2" noWrap>
+                Application
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography
+                variant="subtitle2"
+                display={"flex"}
+                justifyContent={"space-around"}
+                noWrap
+              >
+                Action
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
       </Box>
+
       <Grid style={{ height: "62vh" }}>
         {_DATA
           .currentData()
