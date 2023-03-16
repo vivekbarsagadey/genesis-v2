48f6faacefa53c309a5f48d55641caef7c58d4d3
neha <vivek.bnb@gmail.com>
2023-03-16 16:49:04 +0530

    working grid layout

--- admin-ui/app/company/info/index.tsx
+++ admin-ui/app/company/info/index.tsx
@@ -1,9 +1,10 @@
 "use client";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import { Grid, IconButton, Paper, Tooltip, Typography } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
 import Link from "next/link";
 import { deleteCompany } from "../../../services/company.action";
 type CompanyInfoProps = {
@@ -18,49 +19,39 @@ const InfoCompanyComponent = ({ Items }: CompanyInfoProps) => {
   };
   return (
     <>
-      <Grid mt={0.6} width={"98.2%"}>
+      <Box mt={0.6} mr={2}>
         <Paper variant="outlined">
           <Grid container>
-            <Grid item xs={0.12}></Grid>
-            <Grid item xs={0.6}>
-              <Checkbox size="small" />
-            </Grid>
-            <Grid item xs={1.25}>
-              <Tooltip title="View">
-                <IconButton>
-                  <RemoveRedEyeIcon fontSize="small" />
-                </IconButton>
-              </Tooltip>
-            </Grid>
-            <Grid item xs={2} sm={2} lg={2.2}>
-              <Typography variant="body2">{Items.name}</Typography>
-            </Grid>
-            <Grid item xs={2} lg={1.98}>
-              <Typography variant="body2" noWrap>
-                {Items.email}
-              </Typography>
-            </Grid>
-            <Grid item xs={2.2} sm={2.8} lg={2.43}>
-              <Grid container display={"flex"} alignItems={"center"}>
-                <Grid item xs={12}>
-                  <Typography variant="body2" noWrap>
-                    {Items.mobile}
-                  </Typography>
+            <Grid item xs={2} display={"flex"} justifyContent={"flex-end"}>
+              <Grid container ml={1}>
+                <Grid item xs={4}>
+                  <IconButton>
+                    <CheckBoxOutlineBlankIcon fontSize="small" />
+                  </IconButton>
+                </Grid>
+                <Grid item xs={6}>
+                  <IconButton>
+                    <RemoveRedEyeIcon fontSize="small" />
+                  </IconButton>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item xs={1.5} sm={1.8} lg={2.15}>
-              <Grid container display={"flex"} alignItems={"center"}>
-                <Grid item xs={12}>
-                  <Typography variant="body2" noWrap>
-                    {Items.address}
-                  </Typography>
-                </Grid>
-              </Grid>
+
+            <Grid item xs={2}>
+              <Typography variant="body2" noWrap>{Items.name}</Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography variant="body2" noWrap>{Items.email}</Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography variant="body2" noWrap> {Items.mobile}</Typography>
+            </Grid>
+            <Grid item xs={2} mr={7}>
+              <Typography variant="body2" noWrap>{Items.address}</Typography>
             </Grid>
             <Grid item xs={1}>
               <Grid container>
-                <Grid item xs={3.8}>
+                <Grid item xs={4}>
                   <Tooltip title="Edit">
                     <Link href={`/company/${Items.id}`}>
                       <IconButton>
@@ -69,7 +60,7 @@ const InfoCompanyComponent = ({ Items }: CompanyInfoProps) => {
                     </Link>
                   </Tooltip>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item xs={2}>
                   <Tooltip title="Delete">
                     <IconButton>
                       <DeleteOutlineIcon
@@ -83,7 +74,7 @@ const InfoCompanyComponent = ({ Items }: CompanyInfoProps) => {
             </Grid>
           </Grid>
         </Paper>
-      </Grid>
+      </Box>
     </>
   );
 };
--- admin-ui/app/company/list/list.view.component.tsx
+++ admin-ui/app/company/list/list.view.component.tsx
@@ -1,18 +1,18 @@
 "use client";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import { Box, Grid, IconButton, Pagination, Typography } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
 import Paper from "@mui/material/Paper";
 import { useState } from "react";
 import ICompany from "../company.model";
 import InfoCompanyComponent from "../info";
-import InfoCompany from "../info";
 import CompanyPagination from "./company.list.pagination";
 
-type CompanyComponentProps={
+type CompanyComponentProps = {
   companyData: Array<ICompany>;
   companySearchList: any;
-}
+  Items: any;
+};
 
 const ListViewComponent = ({
   companyData,
@@ -30,36 +30,52 @@ const ListViewComponent = ({
   };
 
   return (
-    <div>
-      <Box mt={2} width={"98.2%"}>
+    <>
+      <Box mr={2} mt={2}>
         <Paper variant="outlined">
           <Grid container>
-            <Grid item xs={0.12}></Grid>
-            <Grid item xs={0.6}>
-              <Checkbox size="small" />
-            </Grid>
-            <Grid item xs={1.25} style={{ display: "flex" }}>
-              <IconButton>
-                <RemoveRedEyeIcon fontSize="small" />
-              </IconButton>
-            </Grid>
-            <Grid item xs={2.2} sm={2.2}>
-              <Typography variant="subtitle2" noWrap>Company Name</Typography>
-            </Grid>
-            <Grid item xs={2} sm={2}>
-              <Typography variant="subtitle2">Email</Typography>
-            </Grid>
-            <Grid item xs={1.8} sm={2.4}>
-              <Typography variant="subtitle2">Contact</Typography>
+            <Grid item xs={2} display={"flex"} justifyContent={"flex-end"}>
+              <Grid container ml={1}>
+                <Grid item xs={4}>
+                  <IconButton>
+                    <CheckBoxOutlineBlankIcon fontSize="small" />
+                  </IconButton>
+                </Grid>
+                <Grid item xs={6}>
+                  <IconButton>
+                    <RemoveRedEyeIcon fontSize="small" />
+                  </IconButton>
+                </Grid>
+              </Grid>
             </Grid>
 
-            <Grid item xs={2.3}>
+            <Grid item xs={2}>
+              <Typography variant="subtitle2" noWrap>
+                Company Name
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography variant="subtitle2" noWrap>
+                Email
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography variant="subtitle2" noWrap>
+                Contact
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
               <Typography variant="subtitle2" noWrap>
                 Address
               </Typography>
             </Grid>
-            <Grid item xs={0.8}>
-              <Typography variant="subtitle2" noWrap>
+            <Grid item xs={2}>
+              <Typography
+                variant="subtitle2"
+                display={"flex"}
+                justifyContent={"space-around"}
+                noWrap
+              >
                 Action
               </Typography>
             </Grid>
@@ -73,7 +89,7 @@ const ListViewComponent = ({
           .filter((ele) =>
             ele.name.toLowerCase().includes(companySearchList.toLowerCase())
           )
-          ?.map((Items, index) => {
+          ?.map((Items: CompanyComponentProps, index) => {
             return (
               <Typography key={index}>
                 <InfoCompanyComponent Items={Items} />
@@ -97,7 +113,7 @@ const ListViewComponent = ({
         </Grid>
         <Grid item xs={0.2}></Grid>
       </Grid>
-    </div>
+    </>
   );
 };
 
