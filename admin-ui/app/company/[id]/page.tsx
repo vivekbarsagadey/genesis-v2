37a7ff74aab1dde88942d7f046a1514852e25631
Saurabh Chandel <saurabh.c@whizit.co.in>
2023-03-24 14:11:51 +0530

    added use() call for fetch updated [id].tsx page data issue

--- admin-ui/app/company/[id]/page.tsx
+++ admin-ui/app/company/[id]/page.tsx
@@ -1,26 +1,39 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import CompanyEditComponent from ".";
+import CompanyEditComponent from "./campany.id";
+import { use } from "react";
 
+type Company = {
+  name: string;
+  address: string;
+  firstName: string;
+  lastname: string;
+  email: string;
+  mobile: string;
+  website: string;
+  foundationYear: string;
+};
 const Page = ({ params }) => {
-  const [company, setCompany] = useState([]);
+  // const [company, setCompany] = useState(null);
   const id = params.id;
-  const fetchData = async () => {
-    const users = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/companies/${id}`
-    );
-    const result = await users.json();
-    setCompany(result);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
 
-  return (
-    <>
-      <CompanyEditComponent company={company} id={id} />
-    </>
-  );
+  const getCompany = fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/companies/${id}`
+  ).then((res) => res.json());
+  const company = use<Company>(getCompany);
+  // const fetchData = async () => {
+  //   const users = await fetch(
+  //     `${process.env.NEXT_PUBLIC_API_URL}/companies/${id}`
+  //   );
+  //   const result = await users.json();
+
+  //   setCompany(result);
+  // };
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  return <>{company && <CompanyEditComponent company={company} id={id} />}</>;
 };
 
 export default Page;
