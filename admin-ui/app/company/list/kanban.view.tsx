8aef988ef25ba633efda805d8d29a0ec76786ccb
Saurabh Chandel <saurabh.c@whizit.co.in>
2023-03-25 14:13:42 +0530

    kanban view updating

--- admin-ui/app/company/list/kanban.view.tsx
+++ admin-ui/app/company/list/kanban.view.tsx
@@ -7,15 +7,40 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid/Grid";
+import { ICompany } from "../models";
 
+type IActiveCompany = {
+  activeCompany: ICompany;
+};
+type INewCompany = {
+  newCompany: ICompany;
+};
+type IInActiveCompany = {
+  inActiveCompany: ICompany;
+};
 const CompanyKanbanView = ({ companies }: ListComponentProps) => {
+  const newCompanies = companies.filter((ele: ICompany) => {
+    return ele.status === "NEW";
+  });
+  const activeCompanies = companies.filter((ele: ICompany) => {
+    return ele.status === "ACTIVE";
+  });
+  const inActiveCompanies = companies.filter((ele: ICompany) => {
+    return ele.status === "INACTIVE";
+  });
+
   return (
     <>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} mt={1}>
         <Grid item xs={4}>
           <Card>
             <CardContent>
               <Typography variant="h6">NEW</Typography>
+              {newCompanies?.map((newCompany, index) => {
+                return (
+                  <NewCompanyComponent newCompany={newCompany} key={index} />
+                );
+              })}
             </CardContent>
           </Card>
         </Grid>
@@ -23,6 +48,14 @@ const CompanyKanbanView = ({ companies }: ListComponentProps) => {
           <Card>
             <CardContent>
               <Typography variant="h6">ACTIVE</Typography>
+              {activeCompanies?.map((activeCompany, index) => {
+                return (
+                  <ActiveCompanyComponent
+                    activeCompany={activeCompany}
+                    key={index}
+                  />
+                );
+              })}
             </CardContent>
           </Card>
         </Grid>
@@ -30,11 +63,41 @@ const CompanyKanbanView = ({ companies }: ListComponentProps) => {
           <Card>
             <CardContent>
               <Typography variant="h6">INACTIVE</Typography>
+              {inActiveCompanies?.map((inActiveCompany, index) => {
+                return (
+                  <InActiveCompanyComponent
+                    inActiveCompany={inActiveCompany}
+                    key={index}
+                  />
+                );
+              })}
             </CardContent>
           </Card>
         </Grid>
       </Grid>
     </>
+  );
+};
+
+const NewCompanyComponent = ({ newCompany }: INewCompany) => {
+  return (
+    <Card >
+      <CardContent>{newCompany.status}</CardContent>
+    </Card>
+  );
+};
+const ActiveCompanyComponent = ({ activeCompany }: IActiveCompany) => {
+  return (
+    <Card>
+      <CardContent>{activeCompany.status}</CardContent>
+    </Card>
+  );
+};
+const InActiveCompanyComponent = ({ inActiveCompany }: IInActiveCompany) => {
+  return (
+    <Card>
+      <CardContent>{inActiveCompany.status}</CardContent>
+    </Card>
   );
 };
 
