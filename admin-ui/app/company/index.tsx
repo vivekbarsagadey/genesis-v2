bc3618bd4d4fcaff5395078e685d5afa46ee52c3
neha <vivek.bnb@gmail.com>
2023-04-05 11:58:18 +0530

    working on company index file

--- admin-ui/app/company/index.tsx
+++ admin-ui/app/company/index.tsx
@@ -46,6 +46,7 @@ const CompanyComponentHome = ({ companies }: CompanyComponentProps) => {
           <Grid item xs={8} md={8} sm={8} lg={8} display={"flex"}>
             <Grid container spacing={1}>
               <Grid item xs={"auto"}>
+                
                 <FilterComponent companies={companies} onFilterHandler={onSearchHandler}/>
               </Grid>
               <Grid item xs={"auto"}>
