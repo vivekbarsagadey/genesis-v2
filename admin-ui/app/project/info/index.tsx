66ddf0e6ee8a325eb820187e45294855a73e9dda
Saurabh Chandel <saurabh.c@whizit.co.in>
2023-04-06 13:33:37 +0530

    working on project builder

--- admin-ui/app/project/info/index.tsx
+++ admin-ui/app/project/info/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState } from "react";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import EditIcon from "@mui/icons-material/Edit";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
@@ -12,15 +13,44 @@ import {
   Typography,
 } from "@mui/material";
 import Link from "next/link";
+import Modal from "@mui/material/Modal";
+import { useRouter } from "next/navigation";
+
 import downloadJsonFile from "../../utility/project/json.downloder";
 
 type CompanyComponentProps = {
   items: any;
 };
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const InfoProjectComponent = ({ items }: CompanyComponentProps) => {
+  const router = useRouter();
+  const [openThemeSelector, setOpenThemeSelector] = useState<boolean>(false);
+
   const jsonFileDownload = () => {
     downloadJsonFile(items);
+  };
+  const editProject = () => {
+    const data = items.projectJson;
+
+    if (data !== null) {
+      router.push(`/project/${items.id}`);
+    } else {
+      setOpenThemeSelector((s) => !s);
+    }
+  };
+  const handleClose = () => {
+    setOpenThemeSelector((s) => !s);
   };
 
   return (
@@ -62,11 +92,9 @@ const InfoProjectComponent = ({ items }: CompanyComponentProps) => {
             <Grid container>
               <Grid item xs={4}>
                 <Tooltip title="Edit">
-                  <Link href={`/project/${items.id}`} passHref>
-                    <IconButton>
-                      <EditIcon fontSize="small" />
-                    </IconButton>
-                  </Link>
+                  <IconButton onClick={editProject}>
+                    <EditIcon fontSize="small" />
+                  </IconButton>
                 </Tooltip>
               </Grid>
               <Grid item xs={2}>
@@ -80,6 +108,21 @@ const InfoProjectComponent = ({ items }: CompanyComponentProps) => {
           </Grid>
         </Grid>
       </Paper>
+      <Modal
+        open={openThemeSelector}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </Box>
   );
 };
