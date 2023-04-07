063c80e26583013a47882f07eb5dd43d43b647a4
neha <vivek.bnb@gmail.com>
2023-04-07 10:30:37 +0530

    working on customer graph view

--- admin-ui/app/customer/list/graph.view.tsx
+++ admin-ui/app/customer/list/graph.view.tsx
@@ -1,24 +1,37 @@
-import React from "react";
 import PieChart from "react-pie-graph-chart";
-import { ICustomer, Status } from "../models";
-
 import { ListComponentProps } from "./props";
-
-const statusSet = Object.keys(Status).filter((v) => isNaN(Number(v)));
 
 const CustomerGraphView = ({ customer }: ListComponentProps) => {
   const companyStatus = [
     {
-      type: "ACTIVE",
-      value: customer.filter((item: ICustomer) => item.status == statusSet[1])
-        .length,
-      color: "#0a9523",
+      type: "India",
+      value: customer?.filter((item) => item.country === "India")?.length,
+      color: "#4d7c0f",
     },
     {
-      type: "INACTIVE",
-      value: customer.filter((item: ICustomer) => item.status == statusSet[2])
-        .length,
-      color: "#f50a0a",
+      type: "Australia",
+      value: customer.filter((item) => item.country === "Australia").length,
+      color: "#62B170",
+    },
+    {
+      type: "America",
+      value: customer.filter((item) => item.country === "America").length,
+      color: "#86198f",
+    },
+    {
+      type: "Spain",
+      value: customer.filter((item) => item.country === "Spain").length,
+      color: "#4BA2DA",
+    },
+    {
+      type: "US",
+      value: customer.filter((item) => item.country === "US").length,
+      color: "#F1AF13",
+    },
+    {
+      type: "UK",
+      value: customer.filter((item) => item.country === "UK").length,
+      color: "#1d4ed8",
     },
   ];
 
