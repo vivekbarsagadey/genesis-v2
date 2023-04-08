19c8b330a42f228d8c654db04c6e82a17214f123
Saurabh Chandel <saurabh.c@whizit.co.in>
2023-04-08 13:55:19 +0530

    upadting main layout section for builder

--- admin-ui/builder/index.tsx
+++ admin-ui/builder/index.tsx
@@ -9,29 +9,33 @@ import Screens from "./screens/screens";
 import BuilderHeaderComponent from "./header/header";
 import BuilderScreenComponent from "./screens/screens";
 import { findById } from "../services/api.service";
+import ScreenHeader from "./screens/screen.header";
 
 const BuilderHome = () => {
   const value = React.useContext(ProjectContext);
-  const project = use(findById("projects", value));
-  console.log(" project -->",project);
-  
+
+  // id fetched Project Data, if Im using use hook then my model is not working poperly
+  // const project = use(findById("projects", value));
+  console.log("genisys >>", genisys);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={2}>
           <>
+            meu bar section
             {/* {genisys.categories?.map((category, index) => {
-          return (
-            <Category
-              key={index}
-              id={index}
-              name={category.name}
-              icon={category.icon}
-              title={category.title}
-              items={category.items}
-            />
-          );
-        })} */}
+              return (
+                <Category
+                  key={index}
+                  id={category.id}
+                  name={category.name}
+                  icon={category.icon}
+                  title={category.title}
+                  items={category.items}
+                />
+              );
+            })} */}
           </>
         </Grid>
         <Grid item xs={10}>
@@ -39,17 +43,18 @@ const BuilderHome = () => {
             <BuilderHeaderComponent />
           </Grid>
           <Grid item xs={12}>
-           
-            <Grid   container spacing={2} >
-
-                <Grid  item xs={12} >Header </Grid>
-                <Grid  item xs={8} >4 different screens</Grid>
-                <Grid  item xs={4} >4 property</Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <ScreenHeader />
+              </Grid>
+              <Grid item xs={8}>
+                4 different screens
+              </Grid>
+              <Grid item xs={4}>
+                4 property
+              </Grid>
             </Grid>
-
-
           </Grid>
-          
         </Grid>
       </Grid>
     </Box>
---
+++ admin-ui/builder/screens/screen.header.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const ScreenHeader = () => {
+  return (
+    <div>jdjkdk</div>
+  )
+}
+
+export default ScreenHeader
