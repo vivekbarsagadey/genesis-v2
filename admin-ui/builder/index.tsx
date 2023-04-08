923574f8dff0993245e002d09e39b122a3516a7e
Saurabh Chandel <saurabh.c@whizit.co.in>
2023-04-08 19:56:25 +0530

    updated screen scetion

--- admin-ui/builder/index.tsx
+++ admin-ui/builder/index.tsx
@@ -1,7 +1,6 @@
 "use client";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import React, { useState } from "react";
+import Box from "@mui/material/Box";
 import { ProjectContext } from "../app/project/info";
 import Logo from "../component/common/Sidebar/logo";
 import genisys from "../data/genisys.json";
@@ -10,14 +9,20 @@ import BuilderSidebarComponent from "./menu/sidebar/builder.sidebar.component";
 import ScreenHeader from "./screens/screen.header";
 import MainPageComponent from "./screens/main.page";
 import Properties from "./screens/properties";
-
+import { Grid, Typography } from "@material-ui/core";
+import CloseIcon from "@mui/icons-material/Close";
+import { Case, Switch } from "react-if";
 const BuilderHome = () => {
   const value = React.useContext(ProjectContext);
 
   const [show, setShow] = useState(true);
-
+  const [screenToggle, setScreenToggle] = useState<string>("");
   const handleMenu = () => {
     setShow(!show);
+  };
+
+  const updateScreen = (typeRec: string) => {
+    setScreenToggle(typeRec);
   };
 
   return (
@@ -33,11 +38,37 @@ const BuilderHome = () => {
           </Grid>
           <Grid item xs={12}>
             <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <ScreenHeader />
+              <Grid item xs={12} style={{ display: "flex" }}>
+                <Typography
+                  variant="subtitle2"
+                  onClick={() => updateScreen("screen1")}
+                >
+                  Screen 1{" "}
+                </Typography>
+                <CloseIcon fontSize="small" />
+                <Typography
+                  variant="subtitle2"
+                  onClick={() => updateScreen("screen2")}
+                >
+                  Screen 2{" "}
+                </Typography>
+                <CloseIcon fontSize="small" />
               </Grid>
+
               <Grid item xs={8}>
-                <MainPageComponent />
+                <Switch>
+                  <Case condition={screenToggle === "screen1"}>
+                    <Grid item xs={12}>
+                      <Typography>1 data</Typography>
+                    </Grid>
+                  </Case>
+
+                  <Case condition={screenToggle === "screen2"}>
+                    <Grid item xs={12}>
+                      <Typography>2 Data</Typography>
+                    </Grid>
+                  </Case>
+                </Switch>
               </Grid>
               <Grid item xs={4}>
                 <Properties />
