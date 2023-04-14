2124c59445035e8e60ed502dfafcee570e3fda12
neha <vivek.bnb@gmail.com>
2023-04-14 18:55:51 +0530

    working on builder screen page add switch case

--- admin-ui/builder/screens/screen.select.component.tsx
+++ admin-ui/builder/screens/screen.select.component.tsx
@@ -3,6 +3,9 @@ import { Box, Grid } from "@material-ui/core";
 import { Case, Switch } from "react-if";
 import { Chip, Paper, Stack } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import Switch, { Case, Default } from "react-switch-case";
+
+
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -21,8 +24,7 @@ const ScreenSelectComponent = ({ screenToggle }) => {
             <Grid item xs={8}>
               <Box mt={2}>
                 <Paper variant="outlined" style={{ height: "75vh" }}>
-                  {/* grid layout  */}
-                  <Box sx={{ flexGrow: 1 }}>
+                  {/* <Box sx={{ flexGrow: 1 }}>
                     <Grid container spacing={2}>
                       <Grid item xs={12}>
                         <Item>xs=12</Item>
@@ -67,10 +69,75 @@ const ScreenSelectComponent = ({ screenToggle }) => {
                     <Grid item xs={2}>
                         <Item>xs=2x1</Item>
                       </Grid>
+
                     <Grid item xs={1}>
                         <Item>xs=1x1</Item>
                       </Grid>
-                  </Box>
+                  </Box> */}
+
+                  <Switch>
+                    <Case value="xs=12 ">
+                      <Grid item xs={12}>
+                        <Item>xs=12</Item>
+                      </Grid>
+                    </Case>
+                    <Case value="xs=6 ">
+                      <Grid item xs={6}>
+                        <Item>xs=6</Item>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <Item>xs=6</Item>
+                      </Grid>
+                    </Case>
+
+                    <Case value="xs=6 ">
+                      <Grid item xs={3}>
+                        <Item>xs=3</Item>
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Item>xs=3</Item>
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Item>xs=3</Item>
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Item>xs=3</Item>
+                      </Grid>
+                    </Case>
+
+                    <Case value="xs=4 ">
+                      <Grid item xs={4}>
+                        <Item>xs=4</Item>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Item>xs=4</Item>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Item>xs=4</Item>
+                      </Grid>
+                    </Case>
+
+                    <Case value="xs=6 ">
+                      <Grid item xs={6}>
+                        <Item>xs=6x1</Item>
+                      </Grid>
+                    </Case>
+                    <Case value="xs=4 ">
+                      <Grid item xs={4}>
+                        <Item>xs=4x1</Item>
+                      </Grid>
+                    </Case>
+                    <Case value="xs=2 ">
+                      <Grid item xs={2}>
+                        <Item>xs=2x1</Item>
+                      </Grid>
+                    </Case>
+                    <Case value="xs=1 ">
+                      <Grid item xs={1}>
+                        <Item>xs=1x1</Item>
+                      </Grid>
+                    </Case>
+                  </Switch>
                 </Paper>
               </Box>
             </Grid>
