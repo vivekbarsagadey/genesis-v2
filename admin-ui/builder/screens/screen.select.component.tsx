2bb0cb04b2def4278383433aa16a1b8915923522
neha <vivek.bnb@gmail.com>
2023-04-14 18:29:21 +0530

    woring on builder screen grid page

--- admin-ui/builder/screens/screen.select.component.tsx
+++ admin-ui/builder/screens/screen.select.component.tsx
@@ -54,11 +54,16 @@ const ScreenSelectComponent = ({ screenToggle }) => {
                       <Grid item xs={4}>
                         <Item>xs=4</Item>
                       </Grid>
-                      <Grid item xs={4}>
-                        <Item>xs=4x1</Item>
+                     
+                     
+                      <Grid item xs={6}>
+                        <Item>xs=6x1</Item>
                       </Grid>
                      
                     </Grid>
+                    <Grid item xs={4}>
+                        <Item>xs=4x1</Item>
+                      </Grid>
                     <Grid item xs={2}>
                         <Item>xs=2x1</Item>
                       </Grid>
