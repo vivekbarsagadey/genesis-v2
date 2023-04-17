0b432363f97a382da682685d7f26f120f2c15e44
Saurabh Chandel <saurabh.c@whizit.co.in>
2023-04-17 15:01:59 +0530

    backend issues fixes for custometer

--- admin-ui/pages/api/customer/index.ts
+++ admin-ui/pages/api/customer/index.ts
@@ -4,22 +4,20 @@ import { getSession } from "next-auth/react";
 import prisma from "../../../utils/prisma";
 import { handleApiMiddleware } from "../middleware";
 
-
 // POST /api/customer
 const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   await handleApiMiddleware(req, res);
   if (req.method === "POST") {
     const result = await prisma.customer.create({
       data: req.body,
-    }); 
+    });
     res.json(result?.id);
   } else if (req.method === "GET") {
-    const companies = await prisma.customer.findMany();
-    res.json(companies);
+    const customer = await prisma.customer.findMany();
+    res.json(customer);
   } else {
     res.status(404).send({ message: "Method is not supported" });
   }
 };
-
 
 export default handle;
