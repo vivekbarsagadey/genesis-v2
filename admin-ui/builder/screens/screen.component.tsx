8a43c4a9ecdb10eed642b8ed29e595b7fbd87c3f
Saurabh Chandel <saurabh.c@whizit.co.in>
2023-04-18 11:58:05 +0530

    added screen api calls

--- admin-ui/builder/screens/screen.component.tsx
+++ admin-ui/builder/screens/screen.component.tsx
@@ -1,18 +1,44 @@
-import { Button, Chip, Grid, Stack } from "@mui/material";
+import React, { useEffect } from "react";
+import { Button, Chip, Grid, Paper, Stack } from "@mui/material";
 
 const ScreenComponent = ({ updateScreen, handleDelete }) => {
+  const [screenInfo, setScreenInfo] = React.useState([]);
+  const fetchData = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/screens`);
+    if (!response.ok) {
+      throw new Error("Data coud not be fetched!");
+    } else {
+      return response.json();
+    }
+  };
+  useEffect(() => {
+    fetchData()
+      .then((res) => {
+        setScreenInfo(res);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, []);
+
   return (
     <>
       <Grid container style={{ background: "#1e293b", padding: "0.3rem" }}>
         <Grid item xs={11}>
           <Stack direction="row" spacing={1}>
-            <Chip
-              label="Screen 1"
-              variant="outlined"
-              onClick={() => updateScreen("screen1")}
-              onDelete={handleDelete}
-              style={{ background: "#e2e8f0" }}
-            />
+            {screenInfo?.map((data, index) => {
+              return (
+                <Chip
+                  key={index}
+                  label={`${data.name}`}
+                  variant="outlined"
+                  onClick={() => updateScreen("screen1")}
+                  onDelete={handleDelete}
+                  style={{ background: "#e2e8f0" }}
+                />
+              );
+            })}
+
             {/* <Chip
             label="Screen 2"
             variant="outlined"
