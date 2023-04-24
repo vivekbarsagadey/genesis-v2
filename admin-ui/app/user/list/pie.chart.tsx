64c1a7f4efff70a2f9458d8c296c70edeb2eed26
neha <vivek.bnb@gmail.com>
2023-04-24 15:47:07 +0530

    working on user graph view

--- admin-ui/app/user/list/pie.chart.tsx
+++ admin-ui/app/user/list/pie.chart.tsx
@@ -54,7 +54,7 @@ const UserPieChart = ({
           <Default>
             <Chart
               chartType="PieChart"
-              data={cityData}
+              data={statusData}
               width={"100%"}
               height={"320px"}
             />
