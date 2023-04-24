49d2db65af47ff1c5fbec6f3b4b72c4f2d9dc434
Pranali Bhasme <pranali.b@whizit.co.in>
2023-04-24 15:46:12 +0530

    added print functionality in project component

---
+++ admin-ui/app/project/print/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { IconButton, Tooltip } from '@mui/material';
+import React from 'react'
+import PrintIcon from '@mui/icons-material/Print';
+
+
+
+
+const PrintComponent = () => {
+   
+   
+  return (
+    <>
+        <Tooltip title='Print'>
+        <IconButton  onClick={() => window.print()}   >
+        <PrintIcon fontSize='small'/>
+        </IconButton>
+        </Tooltip>
+        
+    </>
+  )
+}
+
+export default PrintComponent
