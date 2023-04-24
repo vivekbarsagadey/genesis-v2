67ec983ebe34a735934ce2bfdcd864d6da505e85
neha <vivek.bnb@gmail.com>
2023-04-24 12:47:50 +0530

    working on customer module search-bar on all field

--- admin-ui/app/customer/search/index.tsx
+++ admin-ui/app/customer/search/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 import { ICustomer } from "../models";
 
 interface CustomerSearchComponentProps {
@@ -15,6 +15,9 @@ const CustomerSearchDetails = ({
   const [searchStr, setSearchStr] = useState<string>("");
 
   const filterByName = (firstName: string) =>  (f: ICustomer): boolean => f.firstName.toLowerCase().includes(firstName.toLowerCase());
+  const filterByEmail = (email: string) =>  (f: ICustomer): boolean => f.email.toLowerCase().includes(email.toLowerCase());
+  const filterByMobile = (mobile: string) =>  (f: ICustomer): boolean => f.mobile.toLowerCase().includes(mobile.toLowerCase());
+  const filterByAddress = (address: string) =>  (f: ICustomer): boolean => f.address.toLowerCase().includes(address.toLowerCase());
 
   const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const _searchValue = e.target.value;
@@ -24,8 +27,14 @@ const CustomerSearchDetails = ({
       return;
     }
     onSearchHandler(customer.filter(filterByName(_searchValue)));
+    onSearchHandler(customer.filter(filterByEmail(_searchValue)));
+    onSearchHandler(customer.filter(filterByMobile(_searchValue)));
+    onSearchHandler(customer.filter(filterByAddress(_searchValue)));
   };
 
+
+  console.log("customercustomercustomercustomer",customer);
+  
   return (
     <>
       <Grid item xs={12}>
