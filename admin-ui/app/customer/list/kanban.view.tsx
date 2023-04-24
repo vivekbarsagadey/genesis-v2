a809a17893bda8a0a064f203bb8caf9600c52d0a
Pranali Bhasme <pranali.b@whizit.co.in>
2023-04-25 00:12:13 +0530

    customer kanban view

--- admin-ui/app/customer/list/kanban.view.tsx
+++ admin-ui/app/customer/list/kanban.view.tsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import React,{useState} from 'react'
 import Box from "@mui/material/Box";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid/Grid";
@@ -6,11 +6,32 @@ import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import { ICustomer, Status } from "../models";
 import { ListComponentProps } from "./props";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Card, IconButton, Paper } from "@mui/material";
+import { makeStyles } from '@mui/styles';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+
 
 const CardStyle = styled(Grid)(({ theme }) => ({
   height: "80vh",
   overflowY: "auto",
 }));
+
+const useStyles = makeStyles({
+  cardView:{
+    background:'#F1F6F9',
+  },
+  iconStyle:{
+    padding:'0px'
+  }
+ 
+})
 
 type IActiveCustomer = {
   activeCustomer: ICustomer;
@@ -21,26 +42,74 @@ type IInActiveCustomer = {
 const CustomerKanbanView = ({ customer }: ListComponentProps) => {
   const statusSet = Object.keys(Status).filter((v) => isNaN(Number(v)));
 
-  const activeCompanies = customer.filter((ele: ICustomer) => {
+  const activeCustomers = customer.filter((ele: ICustomer) => {
     return ele.status == statusSet[0];
   });
   const inActiveCustomer = customer.filter((ele: ICustomer) => {
     return ele.status == statusSet[1];
   });
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleDropdownClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  const classes = useStyles();
   return (
     <>
       <Grid container spacing={2} mt={1}>
         <Grid item xs={1}></Grid>
         <Grid item xs={4}>
           <CardStyle>
-            <Paper variant="outlined">
+            <Paper variant="outlined" className={classes.cardView} >
               <CardContent>
-                <Typography variant="h6">ACTIVE</Typography>
-                {activeCompanies.reverse()?.map((activeCustomer, index) => {
+                <Grid container>
+                  <Grid item xs={6} display='flex' alignItems='center'>
+                  <FiberManualRecordIcon fontSize="inherit" htmlColor="green"/>
+                  <Typography variant="h6">ACTIVE</Typography>
+                  </Grid>
+                  <Grid item xs={6}  display='flex' justifyContent='flex-end'>
+                  <IconButton
+                  className={classes.iconStyle}>
+                     <MoreHorizIcon htmlColor='black'
+                     id="basic-button"
+                     aria-controls={open ? 'basic-menu' : undefined}
+                     aria-haspopup="true"
+                     aria-expanded={open ? 'true' : undefined}
+                     onClick={handleClick}
+                     />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleDropdownClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleDropdownClose} >
+          <EditIcon fontSize='small'/>
+          <Typography>Edit</Typography>
+          </MenuItem>
+        <MenuItem onClick={handleDropdownClose} >
+          <DeleteIcon fontSize='small' htmlColor='red'/>
+          <Typography color='red'>Delete</Typography>
+          </MenuItem>
+      </Menu>
+                  </Grid>
+
+
+                </Grid>
+                {activeCustomers.reverse()?.map((activeCustomer, index) => {
                   return (
-                    <ActiveCompanyComponent
-                      activeCustomer={activeCustomer}
+                    <ActiveCustomerComponent
+                    activeCustomer={activeCustomer}
                       key={index}
                     />
                   );
@@ -49,16 +118,48 @@ const CustomerKanbanView = ({ customer }: ListComponentProps) => {
             </Paper>
           </CardStyle>
         </Grid>
-        <Grid item xs={1}></Grid>
         <Grid item xs={4}>
           <CardStyle>
-            <Paper variant="outlined">
+            <Paper variant="outlined" className={classes.cardView}>
               <CardContent>
-                <Typography variant="h6">INACTIVE</Typography>
+              <Grid container >
+                  <Grid item xs={6} display='flex' alignItems='center'>
+                  <FiberManualRecordIcon fontSize="inherit" htmlColor="red"/>
+                  <Typography variant="h6">INACTIVE</Typography>
+                  </Grid>
+                  <Grid item xs={6}  display='flex' justifyContent='flex-end'>
+                    <IconButton className={classes.iconStyle}>
+                    <MoreHorizIcon htmlColor='black'
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}/>
+                    </IconButton>
+                  </Grid>
+                  <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleDropdownClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+       <MenuItem onClick={handleDropdownClose} >
+          <EditIcon fontSize='small'/>
+          <Typography>Edit</Typography>
+          </MenuItem>
+        <MenuItem onClick={handleDropdownClose} >
+          <DeleteIcon fontSize='small' htmlColor='red'/>
+          <Typography color='red'>Delete</Typography>
+          </MenuItem>
+      </Menu>
+                </Grid>
                 {inActiveCustomer.reverse()?.map((inActiveCustomer, index) => {
                   return (
                     <InActiveCustomerComponent
-                      inActiveCustomer={inActiveCustomer}
+                    inActiveCustomer={inActiveCustomer}
                       key={index}
                     />
                   );
@@ -72,34 +173,49 @@ const CustomerKanbanView = ({ customer }: ListComponentProps) => {
   );
 };
 
-const ActiveCompanyComponent = ({ activeCustomer }: IActiveCustomer) => {
+const ActiveCustomerComponent = ({ activeCustomer }: IActiveCustomer) => {
   return (
-    <Box mt={1}>
-      <Paper variant="outlined">
-        <Typography noWrap variant="h5">
-          Company - {activeCustomer.firstName} {activeCustomer.lastName}
+    <Box mt={1} >
+      <Card variant="outlined">
+        <Grid container>
+          <Grid item xs={12} display='flex' alignItems='center' p={2}>
+            <SupportAgentIcon/>
+            <Typography noWrap pl={1}>{activeCustomer.firstName}</Typography>
+            <Typography noWrap pl={1}>{activeCustomer.lastName}</Typography>
+          </Grid>
+          <Grid item xs={12} display= 'flex' pl={2} pb={2} >
+          <Typography noWrap>
+          Status -
         </Typography>
-        <Typography noWrap variant="h5">
-          Status - {activeCustomer.status}
+          <Typography noWrap variant="h6" pl={1}>
+          {activeCustomer.status}
         </Typography>
-      </Paper>
+          </Grid>
+        </Grid>
+      </Card>
     </Box>
   );
 };
 const InActiveCustomerComponent = ({ inActiveCustomer }: IInActiveCustomer) => {
   return (
-    <Box mt={1}>
-      <Paper variant="outlined">
-        <Typography noWrap variant="h5">
-          {" "}
-          Company - {inActiveCustomer.firstName} {inActiveCustomer.lastName}
-        </Typography>
-        <Typography noWrap variant="h5">
-          {" "}
-          Status - {inActiveCustomer.status}
-        </Typography>
-      </Paper>
-    </Box>
+    <Box mt={1} >
+    <Card variant="outlined">
+      <Grid container>
+        <Grid item xs={12} display='flex' alignItems='center' p={2}>
+          <SupportAgentIcon/>
+          <Typography noWrap pl={1}>{inActiveCustomer.firstName}</Typography>
+            <Typography noWrap pl={1}>{inActiveCustomer.lastName}</Typography>        </Grid>
+        <Grid item xs={12} display= 'flex' pl={2} pb={2} >
+        <Typography noWrap>
+        Status -
+      </Typography>
+        <Typography noWrap variant="h6" pl={1}>
+        {inActiveCustomer.status}
+      </Typography>
+        </Grid>
+      </Grid>
+    </Card>
+  </Box>
   );
 };
 
