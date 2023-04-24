c3a99e238458921840ca44060d9937679f66a895
neha <vivek.bnb@gmail.com>
2023-04-24 15:14:07 +0530

    working on company search bar

--- admin-ui/app/company/search/index.tsx
+++ admin-ui/app/company/search/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-import {ICompany} from "../models/company.model";
+import { useState } from "react";
+import { ICompany } from "../models/company.model";
 
 interface CompanySearchComponentProps {
   companies: Array<ICompany>;
@@ -15,6 +15,9 @@ const CompanySearchDetails = ({
   const [searchStr, setSearchStr] = useState<string>("");
 
   const filterByName = (name: string) =>  (f: ICompany): boolean => f.name.toLowerCase().includes(name.toLowerCase());
+  const filterByEmail = (email: string) =>  (f: ICompany): boolean => f.email.toLowerCase().includes(email.toLowerCase());
+  // const filterByContact = (mobile: number) =>  (f: ICompany): boolean => f.mobile.toLowerCase().includes(mobile.toLowerCase());
+  const filterByAddress = (address: string) =>  (f: ICompany): boolean => f.address.toLowerCase().includes(address.toLowerCase());
 
   const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const _searchValue = e.target.value;
@@ -24,7 +27,13 @@ const CompanySearchDetails = ({
       return;
     }
     onSearchHandler(companies.filter(filterByName(_searchValue)));
+    onSearchHandler(companies.filter(filterByEmail(_searchValue)));
+    // onSearchHandler(companies.filter(filterByContact(_searchValue)));
+    onSearchHandler(companies.filter(filterByAddress(_searchValue)));
   };
+
+
+console.log("companies",companies)
 
   return (
     <>
