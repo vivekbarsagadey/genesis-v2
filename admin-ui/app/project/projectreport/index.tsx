657c19ed7ed216093f01a09cc37d4519a2f038c6
Pranali Bhasme <pranali.b@whizit.co.in>
2023-04-27 12:45:24 +0530

    project report

--- admin-ui/app/project/projectreport/index.tsx
+++ admin-ui/app/project/projectreport/index.tsx
@@ -6,24 +6,24 @@ import TableRenderers from 'react-pivottable/TableRenderers';
 import createPlotlyRenderers from 'react-pivottable/PlotlyRenderers';
 import createPlotlyComponent from 'react-plotly.js/factory';
 import Plot from 'react-plotly.js';
+import moment from 'moment';
+
 // const Plot = createPlotlyComponent(window.Plotly);
 const PlotlyRenderers = createPlotlyRenderers(Plot);
 export default function ReportButtonComponent({ projects }: any) {
   const [settings, setSettings] = useState({});
   console.log('projects >>at report ', projects);
+  const date = moment(new Date()).format('DD/MM/YYYY');
+
+ 
 
   return (
     <>
       <PivotTableUI
-        data={projects}
+        data={data}
         onChange={(s) => setSettings(s)}
-        cols={['createdAt',]}
-        rows={[
-          'name',
-          'customerName',
-          'application',
-          'status',
-        ]}
+        cols={['createdAt']}
+        rows={['name', 'customerName', 'application', 'status']}
         vals={['aos_pax_total']}
         aggregatorName="Integer Sum"
         renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
@@ -34,7 +34,6 @@ export default function ReportButtonComponent({ projects }: any) {
           'pvtVals',
           'pvtAxisContainer',
         ]}
-        
       />
     </>
   );
